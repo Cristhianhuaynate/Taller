4f2e6d0fa576e36302802a03a9c1a25c018edddd
Cristhianhuaynate <118915469+Cristhianhuaynate@users.noreply.github.com>
2022-11-30 11:46:25 -0500

    Add files via upload

---
+++ taller1.js
@@ -0,0 +1,10 @@
+// Calcula la suma de los n primeros numeros enteros utilizando la estructura fo n, suma;
+
+let n = prompt("Ingrese hasta que numero desea calcular la suma de los numeros");
+let suma=0;
+
+
+for(let i=1;i<=n;i++){
+    suma += i;
+    console.log(suma);
+}git push -u origin main
